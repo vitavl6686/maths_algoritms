1f997018672c1a42abd57a789a23c07f50556273
vitavl6686 <vita@trousev.pro>
2021-11-26 17:28:56 +0000

    stacks and queues via Linked Lists and resizing arrays

---
+++ src/ie/vita/StacksAndQueues/EmptyArrayException.java
@@ -0,0 +1,12 @@
+package ie.vita.StacksAndQueues;
+
+public class EmptyArrayException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6146606034088393534L;
+	public EmptyArrayException() {}
+	public EmptyArrayException(String message) {
+		super(message);
+	}
+}
