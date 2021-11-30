b10c7c1d27b4760935ddcc078ef9418a0b89fd01
vitavl6686 <vita@trousev.pro>
2021-11-30 19:06:28 +0000

    generics and iterators in queues ans stacks. Bag

--- src/ie/vita/StacksAndQueues/QueueUsingLinkedList.java
+++ src/ie/vita/StacksAndQueues/QueueUsingLinkedList.java
@@ -1,42 +1,42 @@
 /* This class provides an implementation
  * of a queue via Linked List
  * 
- * private class Node is one item of the queue
- * It contains  link to laster node and info.
- * 
- * QueueUsingLinkedList queue = new QueueUsingLinkedList();
- * 
- * queue.inqueue(string) adds string to the queue.
- * returns NOTHING
- * 
- * queue.dequeue() delete the first item, return it.
- * Returns the first item (String)
- * 
- * queue.isEmpty() returns BOOLEAN (is this queue empty)
- * 
+ * Use:
+ * QueueUsingLinkedList<Item> queue = new QueueUsingLinkedList<>();
  *  
  * */
 
 
 package ie.vita.StacksAndQueues;
+import java.util.Iterator;
 
-public class QueueUsingLinkedList {
+public class QueueUsingLinkedList<Item> implements Iterable<Item>{
 	private Node first = null;
 	private Node last = null;
 	
 	private class Node {
 		Node next;
-		String item;
+		Item item;
 	}
 	
+	/* Returns BOOLEAN
+	 * is this queue empty
+	 * Use:
+	 * queue.isEmpty();
+	 * */
 	public boolean isEmpty() {
 		return first == null;
 	}
 	
-	public void enqueue(String str) {
+	/* Returns NOTHING
+	 * Add the item to the queue
+	 * Use:
+	 * queue.enqueue(item);
+	 * */
+	public void enqueue(Item item) {
 		Node oldLast = last;
 		last = new Node();
-		last.item = str;
+		last.item = item;
 		last.next = null;
 		if (isEmpty()) {
 			first = last;
@@ -45,14 +45,43 @@ public class QueueUsingLinkedList {
 
 	}
 
-
-	public String dequeue() {
-		String answer = first.item;
+	/* Returns Item
+	 * The first item of the queue
+	 * And delete it
+	 * Use:
+	 * queue.dequeue();
+	 * */
+	public Item dequeue() {
+		Item answer = first.item;
 		first = first.next;
 		if (isEmpty()) {
 			last = null;
 		}
 		return answer;
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		return new QueueIterator();
+	}
+	
+	public class QueueIterator implements Iterator<Item> {
+		private Node current = first;
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public Item next() {
+			if (!this.hasNext()) {
+				throw new NoNextElementException();
+			}
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+		
 	}
 	
 	
