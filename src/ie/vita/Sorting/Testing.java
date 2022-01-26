a62f00de4fe96c5c8930a2750fdcbfed888b05f6
vitavl6686 <vita@trousev.pro>
2022-01-26 16:01:35 +0000

    mergesort

--- src/ie/vita/Sorting/Testing.java
+++ src/ie/vita/Sorting/Testing.java
@@ -2,12 +2,12 @@ package ie.vita.Sorting;
 
 public class Testing {
 	public static void main(String[] args) {
-		Integer[] arr = new Integer[] {0, 8, 6, 4,  2, 5};
+		Integer[] arr = new Integer[] {0, 8, 6, 4,2,  2, 5};
 		for (int i = 0; i<arr.length; i++) {
 			System.out.print(arr[i]);
 		}
 		System.out.println();
-		ShellSort.sort(arr);
+		MergeSort.sort(arr);
 		for (int i = 0; i<arr.length; i++) {
 			System.out.print(arr[i]);
 		}
