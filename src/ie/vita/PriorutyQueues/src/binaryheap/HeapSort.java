076db349face9f6c967e44eefaf785570056c07b
vitavl6686 <vita@trousev.pro>
2022-07-08 18:22:27 +0100

    heap sort next part

--- src/ie/vita/PriorutyQueues/src/binaryheap/HeapSort.java
+++ src/ie/vita/PriorutyQueues/src/binaryheap/HeapSort.java
@@ -1,9 +1,17 @@
 package binaryheap;
 
+import elementary.ComparingMethods;
+
 public class HeapSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] arr = new String[] {"A", "D", "F", "T", "Z"};
+
+		
+		sort(arr);
+		for (String a : arr) {
+			System.out.println(a);
+		}
 
 	}
 	
@@ -11,11 +19,30 @@ public class HeapSort {
 		int N = array.length - 1; //the last element pointer
 		for(int index = (N-1)/2; index >= 0; index--) {
 			sink(array, index, N);
-		} 
+		}
+		
+		for (int i = N; i > 0; i--) {
+			Comparable temp = array[0];
+			array[0] = array[i];
+			array[i] = temp;
+			i--;
+			sink(array, 0, i);
+		}
 	}
 	
-	private static void sink() {
-		
+	private static void sink(Comparable[] array, int index, int N) {
+		while(2 * index + 1 <= N) {
+			int j = 2 * index + 1;
+			if (j+1 <= N && ComparingMethods.less(array[j], array[j+1])) {
+				j = j + 1;
+			}
+			if (ComparingMethods.less(array[index], array[j])) {
+				Comparable temp = array[index];
+				array[index] = array[j];
+				array[j] = temp;
+			}
+			index = j;
+		}
 	}
 
 }
